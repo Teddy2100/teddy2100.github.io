2da2bcd77ee5c048f9d27bedc70f4d34d5a5442a
aronfeerick <aronfeerick@gmail.com>
2015-07-23 17:46:27 +0100

    Update 2015-07-23T16:45:18.842Z

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ var ghPages=require('gulp-gh-pages');
 gulp.task("deploy",function(){
  var options={branch:"master",cacheDir:".page"};
 
+ var send=["./**/*","!./node_modules/**","!./required/**"];
+
  return gulp.src("./**/*").pipe(ghPages(options));
 });
 
