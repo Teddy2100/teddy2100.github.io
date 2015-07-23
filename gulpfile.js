c96e14862055018bb4524ced192f8c9fffb921c8
aronfeerick <aronfeerick@gmail.com>
2015-07-23 17:26:33 +0100

    Update 2015-07-23T16:25:10.248Z

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,8 @@ var gulp=require('gulp');
 var ghPages=require('gulp-gh-pages');
  
 gulp.task("deploy",function(){
- var options={remoteUrl:"https://github.com/aronfeerick/aronfeerick.github.io.git",branch:"master",cacheDir:".page"}
+ var options={branch:"master",cacheDir:".page"};
+
  return gulp.src("./**/*").pipe(ghPages(options));
 });
 
